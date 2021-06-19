78aca638ef81d47c9494a32ff8b2619d56474105
p-kharitonov <workingpavel@mail.ru>
2021-06-20 01:09:36 +0300

    v0.8

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Проект NewsPortal</h1>
+<hr>
+<p>sudo apt install python3-pip</p>
+<p>pip3 install -r requirements.txt/p>
+<hr>
+<p>sudo add-apt-repository ppa:redislabs/redis/p>
+<p>sudo apt-get update/p>
+<p>sudo apt-get install redis/p>
+<hr>
+<p>pip3 install -U "celery[redis]"/p>
+<p>redis-server</p>
+<hr>
+<p>celery -A mcdonalds worker -l INFO -B</p>
+<hr>
+<p>python3 manage.py makemigrations</p>
+<p>python3 manage.py migrate</p>
+<p>python3 manage.py createsuperuser</p>
+<hr>
+<p>python3 manage.py runserver</p>
---
+++ newsportal/news/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+from django.views.decorators.cache import cache_page
+
+urlpatterns = [
+    path('', cache_page(60*5)(PostListView.as_view()), name='news'),
+    path('<int:pk>', PostDetailView.as_view(), name='post'),
+    path('search/', PostSearchView.as_view(), name='post_search'),
+    path('add/', PostAddView.as_view(), name='post_add'),
+    path('<int:pk>/edit', PostUpdateView.as_view(), name='post_edit'),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
+    path('upgrade/', upgrade_me, name='upgrade'),
+    path('categories/', CategoriesListView.as_view(), name='categories_list'),
+    path('categories/<int:pk>', cache_page(60*5)(PostsCategoryListView.as_view()), name='posts_category'),
+    path('subscribe/', subscribe, name='subscribe'),
+]
